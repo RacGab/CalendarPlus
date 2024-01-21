dd1e8a2e9c16959480b9e0b1e3abd5d947ddbd6b
RacGab <2051697@cshawi.ca>
2024-01-21 01:16:39 -0500

    Added draggable dialog window

--- script.js
+++ script.js
@@ -54,6 +54,41 @@ function showDialog(e, dayKey, eventText) {
     }
 }
 
+dialog.addEventListener('dragstart', function(e) {
+    e.preventDefault();
+});
+
+var pos1 = 0, pos2 = 0, pos3 = 0, pos4 = 0;
+
+dialog.onmousedown = dragMouseDown;
+
+function dragMouseDown(e) {
+    // get the mouse cursor position at startup:
+    pos3 = e.clientX;
+    pos4 = e.clientY;
+    document.onmouseup = closeDragElement;
+    // call a function whenever the cursor moves:
+    document.onmousemove = elementDrag;
+}
+
+function elementDrag(e) {
+    // calculate the new cursor position:
+    pos1 = pos3 - e.clientX;
+    pos2 = pos4 - e.clientY;
+    pos3 = e.clientX;
+    pos4 = e.clientY;
+    // set the element's new position:
+    dialog.style.top = (dialog.offsetTop - pos2) + "px";
+    dialog.style.left = (dialog.offsetLeft - pos1) + "px";
+}
+
+function closeDragElement() {
+    /* stop moving when mouse button is released:*/
+    document.onmouseup = null;
+    document.onmousemove = null;
+}
+
+
 function updateLocalStorage(dayKey, oldEvent, newEvent) {
     const savedEvents = JSON.parse(localStorage.getItem(dayKey) || '[]');
     const index = savedEvents.indexOf(oldEvent);
